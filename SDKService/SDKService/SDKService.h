1d5ab1900331324c838c0b9e9708306a7117861a
acv-ninhnm <ninhnm@atmarkcafe.org>
2018-04-18 10:28:27 +0700

    add source framework

---
+++ SDKService/SDKService/SDKService.h
@@ -0,0 +1,19 @@
+//
+//  SDKService.h
+//  SDKService
+//
+//  Created by NinhNM_ACV on 4/17/18.
+//  Copyright © 2018 JuniorStudio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SDKService.
+FOUNDATION_EXPORT double SDKServiceVersionNumber;
+
+//! Project version string for SDKService.
+FOUNDATION_EXPORT const unsigned char SDKServiceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SDKService/PublicHeader.h>
+
+
---
+++ SDKService/SDKService/SDKService.swift
@@ -0,0 +1,9 @@
+//
+//  SDKService.swift
+//  SDKService
+//
+//  Created by NinhNM_ACV on 4/17/18.
+//  Copyright © 2018 JuniorStudio. All rights reserved.
+//
+
+import Foundation
